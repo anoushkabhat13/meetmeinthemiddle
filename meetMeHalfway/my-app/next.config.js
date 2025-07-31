42549ead117b309c78d9e4b8b574128b6ca55228
anoushkabhat13 <50841365+anoushkabhat13@users.noreply.github.com>
2025-07-31 01:25:52 -0700

    Update and rename next.config.js to next.config.mjs

--- meetMeHalfway/my-app/next.config.js
+++
@@ -1,7 +0,0 @@
-// meetMeHalfway/my-app/next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "standalone", // <--- ADD THIS LINE
-  };
-  
-  module.exports = nextConfig; // Ensure you're using CommonJS export if it's the default
